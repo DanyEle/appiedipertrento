cd083bea875df139a74a0d51ab27ba29e3faeb3e
filippo <filippo.quattrocchi@live.it>
2014-01-11 17:01:19 +0100

    AdjList.py legge i punti

--- Server/AdjList.py
+++ Server/AdjList.py
@@ -12,6 +12,7 @@ lyr.ResetReading()
 
 listaLinee = []
 
+points = []
 AdiList = []
 
 for feat in lyr:
@@ -24,15 +25,26 @@ for i in range(0, len(listaLinee)-1):
     start0y = geom0.GetY(0)
     end0x = geom0.GetX(geom0.GetPointCount()-1)
     end0y = geom0.GetY(geom0.GetPointCount()-1)
+    startPointTouched=0
+    endPointTouched=0
     for j in range(i+1, len(listaLinee)):
         geom1 = listaLinee[j].GetGeometryRef()
         start1x = geom1.GetX(0)
         start1y = geom1.GetY(0)
         end1x = geom1.GetX(geom1.GetPointCount()-1)
         end1y = geom1.GetY(geom1.GetPointCount()-1)
-        if ((start0x==start1x and start0y==start1y) or (start0x==end0x and start0y==end0y) or (end0x==start1x and end0y==start1y) or (end0x==end1x and end0y==end1y)):
+        if ((start0x==start1x and start0y==start1y) or (start0x==end1x and start0y==end1y)):
             AdiList[i].append(j)
             AdiList[j].append(i)
+            startPointTouched=1
+        if (end0x==start1x and end0y==start1y) or (end0x==end1x and end0y==end1y):
+            AdiList[i].append(j)
+            AdiList[j].append(i)
+            endPointTouched=1
+        if startPointTouched==0:
+            points.append([start0x,start0y])
+        if endPointTouched==0:
+            points.append([end0x,end0y])
 
 for i in range(0, len(listaLinee)):
     print AdiList[i]
