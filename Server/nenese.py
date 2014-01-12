dc6520af101eeceb9c42fe6754dc6c3890a80948
filippo <filippo.quattrocchi@live.it>
2014-01-12 23:43:09 +0100

    English translation of nenese.py's comments

--- Server/nenese.py
+++ Server/nenese.py
@@ -1,8 +1,8 @@
 import numpy
 
-#poi è la lista dei punti. Ogni lista contiene una lista di 2 elementi: le coordinate x e y
-#sou e una lista di due elementi: le coordinate x e y del punto di cui cercare il vicino
-#la funzione ritorna la posizione in poi del punto più vicino a sou
+#poi is the list of the points. Each list contains 2 elements: x and y of the point
+#sou is a list of 2 elements: x and y of the points whose nearest neighbor must be found
+#the function returns the IP of the nearest neighbor of sou
 
 def nns(poi, sou):
 	points = numpy.array(poi)
