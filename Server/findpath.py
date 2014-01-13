6446063f18061d676a594b12b4c010d653fd6124
filippo <filippo.quattrocchi@live.it>
2014-01-14 00:03:14 +0100

    Changed a variable's name

--- Server/findpath.py
+++ Server/findpath.py
@@ -6,9 +6,9 @@ locP=0
 locList=[]
 # l is the length of the path
 # P is the pollution amount
-# D is the "target length"
+# T is the "target length"
 
-def findpath(adilist,roads,analizedPoint,endPoint,D,maxLength):
+def findpath(adilist,roads,analizedPoint,endPoint,T,maxLength):
 	global finalList
 	global l
 	global P
@@ -17,13 +17,13 @@ def findpath(adilist,roads,analizedPoint,endPoint,D,maxLength):
 	global locList
 	# finalPoint's memorized list's update (the memorized list is updated when locList is better)
 	if analizedPoint==endPoint:
-		if (abs(locl-D)*locP<abs(l-D)*P) or P==0:
+		if (abs(locl-T)*locP<abs(l-T)*P) or P==0:
 			l=locl
 			P=locP
 			finalList=locList[:]
 		
 	# step
-	if (locl<D or (abs(locl-D)*locP<abs(l-D)*P and (maxLength==-1 or locl<maxLength))):
+	if (locl<T or (abs(locl-T)*locP<abs(l-T)*P and (maxLength==-1 or locl<maxLength))):
 		for adi in adilist[analizedPoint]:
 			edge = roads[adi]
 			if edge[4]==0: # if not touched
@@ -32,9 +32,9 @@ def findpath(adilist,roads,analizedPoint,endPoint,D,maxLength):
 				locl+=edge[2] # longer path
 				locP+=edge[3] # more polluted path
 				if analizedPoint==edge[0]:
-					findpath(adilist,roads,edge[1],endPoint,D)
+					findpath(adilist,roads,edge[1],endPoint,T)
 				if analizedPoint==edge[1]:
-					findpath(adilist,roads,edge[0],endPoint,D)
+					findpath(adilist,roads,edge[0],endPoint,T)
 				locList.pop()
 				locl-=edge[2]
 				locP-=edge[3]
