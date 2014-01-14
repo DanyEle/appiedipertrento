e1e5aea756886f1f20622695ffd009d7198b077b
filippoqua <filippo.quattrocchi@live.it>
2014-01-14 13:30:37 +0100

    Create findpath tester.py

---
+++ Server/findpath tester.py
@@ -0,0 +1,31 @@
+import findpath
+A = []
+B = []
+nV = input("How many vertices? ")
+vE = input("How many edges? ")
+S = input("Start: ")
+E = input("End: ")
+L = input("Target length: ")
+M = input("Maximum length: ")
+
+for i in range(nV):
+	nN = input("How many edges near vertex ", i, " ?")
+	A.append([])
+	for j in range(nN):
+		N = input("Neigbor: ")
+		A[len(A)-1].append(N)
+
+for i in range(nE):
+	B.append([])
+	s = input("start: ")
+	B[len(B)-1].append(s)
+	e = input("end: ")
+	B[len(B)-1].append(e)
+	l = input("length: ")
+	B[len(B)-1].append(l)
+	p = input("pollution amount: ")
+	B[len(B)-1].append(p)
+	B[len(B)-1].append(0)
+	B[len(B)-1].append(-1)
+	
+print findpath.findpath(A,B,S,E,L,M)
