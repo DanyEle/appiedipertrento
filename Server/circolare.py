ed67626dde2590f669d7573edee67fe6cb187c25
filippoqua <filippo.quattrocchi@live.it>
2014-03-29 21:00:20 +0100

    Create circolare.py

---
+++ Server/circolare.py
@@ -0,0 +1,16 @@
+import findpath_edges_dijkstra_length
+import findpath_edges_dijkstra
+
+def circolare(edges, start, length):
+	S = []
+	path = []
+	path2 = []
+	finalPath = []
+	path = findpath_edges_dijkstra_length.findpath(edges, start, length, S)
+	end = path.pop(len(path)-1)
+	if len(path)>0:
+		path.pop(0)
+	path2 = findpath_edges_dijkstra.findpath(edges, end, start, path)
+	finalPath = [start] + path + path2
+	finalPath.pop(len(finalPath)-1)
+	return finalPath
