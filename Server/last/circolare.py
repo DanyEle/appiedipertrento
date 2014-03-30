e1e62c9e47c2c0e77431c8fb661c96279b9e9494
filippoqua <filippo.quattrocchi@live.it>
2014-03-30 20:53:06 +0200

    Update circolare.py

--- Server/last/circolare.py
+++ Server/last/circolare.py
@@ -2,11 +2,10 @@ import findpath_edges_dijkstra_length
 import findpath_edges_dijkstra
 
 def circolare(edges, start, length):
-	S = []
 	path = []
 	path2 = []
 	finalPath = []
-	path = findpath_edges_dijkstra_length.findpath(edges, start, length, S)
+	path = findpath_edges_dijkstra_length.findpath(edges, start, length/2, [])
 	end = path.pop(len(path)-1)
 	if len(path)>0:
 		path.pop(0)
